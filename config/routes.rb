ba4a5345ae41afe3ae50ccb625f630e212fc7ff3
shi-n-ya411 <180539615+shi-n-ya411@users.noreply.github.com>
2024-11-04 11:32:24 +0000

    [ADD]books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  root to: "homes#top"
   devise_for :users
+  resources :post_images, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
